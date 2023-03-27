3e1889b40861072234bf6f27228d83366564cb0f
glenmcgee <37674523+glenmcgee@users.noreply.github.com>
2023-03-27 12:26:40 -0400

    misspecification sims

---
+++ 04-supplement-misspecification.R
@@ -0,0 +1,551 @@
+## Simulations ##
+set.seed(456321)
+
+## Load libraries ----
+# The mam package
+# install.packages('devtools')
+# devtools::install_github('awstringer1/mam')
+library(mam)
+library(mgcv)
+library(gamm4)
+library(tidyverse)
+theme_set(theme_bw())
+theme_replace(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+GGPLOTTEXTSIZE <- 26
+PLOTWIDTH <- PLOTHEIGHT <- 7
+
+library(parallel)
+options(mc.cores = parallel::detectCores())
+
+## Set paths ----
+globalpath <- tempdir()
+plotpath <- file.path(globalpath,"figures")
+if (!dir.exists(plotpath)) dir.create(plotpath)
+savepath <- file.path(globalpath,"data")
+if (!dir.exists(savepath)) dir.create(savepath)
+
+## Set global parameters ----
+f1temp <- function(x){(1.5*sin(pi*x)-2*x)}
+f1 <- function(x) f1temp(x)-f1temp(0)
+f2temp <- function(x){5*(dnorm(2*(x)))}
+f2 <- function(x) f2temp(x)-f2temp(0)
+beta3 <- 0
+
+## random effects drawn from mixture of 2 gaussians
+sigma0 <- 1
+sigma1 <- 3
+nn <- 5000
+mix <- rbinom(nn,1,0.5)
+yy0 <- rnorm(nn,0,sigma0)
+yy1 <- rnorm(nn,0,sigma1)
+yy <- (1-mix)*yy0+mix*yy1 ## mixture of 2 normals
+xx <- rnorm(nn) ## normal distribution with same variance
+
+## histograms
+p1 <- hist(yy,breaks=20)                     
+p2 <- hist(xx,breaks=p1$breaks)   
+plot( p2, col=rgb(0,0,1,0.5), xlim=c(-10,10),ylim=c(0,max(c(p1$counts,p2$counts))))  # first histogram
+plot( p1, col=rgb(1,0,0,0.5), xlim=c(-10,10),ylim=c(0,max(c(p1$counts,p2$counts))), add=T)
+
+## QQ plot
+qqplot(y=yy,
+       x=xx,
+       xlab = "Normal",
+       ylab = "Heavy-tailed Mixture")
+abline(0,1,lty=2)
+
+
+
+get_data_misp <- function(conditional,x1,x2,x3,K,Nk){
+  mix <- rbinom(K,1,0.5)
+  u0 <- rnorm(K,0,sigma0)
+  u1 <- rnorm(K,0,sigma1)
+    V <- as.data.frame((1-mix)*u0+mix*u1)
+    colnames(V) <- "intercepts"
+    V$id <- 1:K
+  dat <- data.frame(id = rep(1:K,each=Nk))
+  dat <- merge(dat,V,by="id",all.x=TRUE)
+  dat$x1 <- x1
+  dat$x2 <- x2
+  dat$x3 <- x3
+  dat$fx1 <- f1(x1)
+  dat$fx2 <- f2(x2)
+  dat$mix <- rep(mix,each=Nk)
+  
+  Ui <- dat[,c("intercepts")]
+
+  if(conditional==FALSE){
+    ## approximation from Hedeker
+    vars <- (1-dat$mix)*sigma0^2+dat$mix*sigma1^2+##SSmat[1,1]+ ##
+      +(pi^2)/3 # (pi^2)/3 is approximate due to logistic distn
+    
+    estar <- rlogis(K*Nk) # approx: rnorm(K*Nk,0,sqrt((pi^2)/3))
+    dat <- within(dat,{y <- as.numeric(pnorm(estar+Ui,0,sqrt(vars)) < mam::ilogit(fx1 + fx2 +               ## non linear fixed effects
+                                                                                    beta3*x3 +                ## linear fixed effects
+                                                                                    0))  })  ## random effects on other side
+  }else{
+    ## generate from GAMM
+    dat <- within(dat,{y <- rbinom(K*Nk,1,mam::ilogit(fx1 + fx2 + beta3*x3 + Ui))})
+                                                                                    
+  }
+
+  return(dat)
+}
+
+do_simulation_misp <- function(K,Nk,conditional,iter=0,verbose=TRUE) {
+
+  ## generate data
+  x1 <- round(runif(K*Nk,-1,1),3)
+  x3 <- round(runif(K*Nk,-1,1),3)
+  x2 <- round(runif(K*Nk,-1,1),3)
+  dat <- get_data_misp(conditional,x1,x2,x3,K,Nk)
+  gridlen=100
+  dat2pred <- data.frame(x1 = c(seq(-1,1,length=gridlen),rep(0,2*gridlen)),
+                         x2 = c(rep(0,gridlen),seq(-1,1,length=gridlen),rep(0,gridlen)),
+                         x3 = c(rep(0,2*gridlen),seq(-1,1,length=gridlen)))
+  dat2pred$fx1 <- f1(dat2pred$x1)
+  dat2pred$fx2 <- f2(dat2pred$x2)
+  
+  ## fit models
+ 
+  # MAM (proposed approach)
+  themam <- mam(smooth = list(s(x1),s(x2)),
+                re = y ~ (1|id),
+                fe = ~ x3,
+                dat = dat,
+                margdat = dat,
+                preddat = dat2pred,
+                control = mam_control(
+                  # method = 'trust',
+                  method = 'BFGS',
+                  varmethod = 1,
+                  verbose = verbose,
+                  retcond = TRUE))
+
+  ## using our own GAMM implementation since gamm4 is the bottleneck (and performs the same as ours in main simulations)
+  # # (vi) GAMM (conditional)
+  # gamm <- gamm4(y ~ s(x1)+s(x2)+x3,random=~(1|id), data = dat,family = binomial())
+  # gammfit <- predict(gamm$gam,newdata=dat2pred,se.fit=TRUE)
+  # gammfitted <- gammfit$fit
+  # gammfitted_se <- gammfit$se.fit
+  # gammpred <- ranef(gamm$mer)$id
+  # if (verbose) cat("Fitting gamm took",as.numeric(difftime(Sys.time(),tm,units='secs')),"seconds.\n")
+
+  ## get fitted values
+  fitteddat <- as_tibble(dat2pred) %>%
+    mutate(
+      ## Simulation parameters
+      K=K,Nk=Nk,conditional,iter=iter,
+      ## mam
+      mamcondfitted = themam$conditional$fitted,
+      mamcondse = themam$conditional$fitted_se,
+      mamcondlower = mamcondfitted - 1.96*mamcondse,
+      mamcondupper = mamcondfitted + 1.96*mamcondse,
+      mamcondcovrx1 = fx1 >= mamcondlower & fx1 <= mamcondupper,
+      mamcondcovrx2 = fx2 >= mamcondlower & fx2 <= mamcondupper,
+      mamcondbiasx1 = mamcondfitted - fx1,
+      mamcondbiasx2 = mamcondfitted - fx2,
+      mammargfitted = as.numeric(themam$mam$fitted),
+      mammargse = as.numeric(themam$mam$fitted_se),
+      mammarglower = mammargfitted - 1.96*mammargse,
+      mammargupper = mammargfitted + 1.96*mammargse,
+      mammargcovrx1 = fx1 >= mammarglower & fx1 <= mammargupper,
+      mammargcovrx2 = fx2 >= mammarglower & fx2 <= mammargupper,
+      mammargbiasx1 = mammargfitted - fx1,
+      mammargbiasx2 = mammargfitted - fx2,
+      iter=iter
+    )
+  
+  ## get random effects
+  b0 <- dat$intercepts[seq(1,nrow(dat),by=Nk)] ## cluster level random intercept
+  
+  ## mam
+  mamMSEPb0 <- mean((themam$conditional$predU[,1]-b0)^2)
+  
+  # ## gamm
+  # gammMSEPb0 <- mean((gammpred[,1]-b0)^2)
+
+  
+  preddat <- data.frame(
+    K=K,Nk=Nk,conditional,iter=iter,
+    mamMSEPb0=mamMSEPb0#,  
+    # mamMSEPb1=mamMSEPb1,  
+    # mamMSEPbki=mamMSEPbki,
+    # gammMSEPb0=gammMSEPb0,
+    # gammMSEPb1=gammMSEPb1,
+    # gammMSEPbki=gammMSEPbki  
+  )
+  
+  
+  ## misspecified
+  # ## estimate variance components
+  # mamtheta <-c(sqrt(themam$conditional$theta[1,1]))
+  # gammtheta <- c(sqrt(VarCorr(gamm$mer)$id[1,1]))
+  # names(mamtheta) <- names(gammtheta) <- c("sd-Intercept","cor","sd-x3")
+  # 
+  # varcomp <- data.frame(
+  #   K=K,Nk=Nk,sigma0=sigma0,sigma3=sigma3,rho=rho,iter=iter,
+  #   mam_sd_int=mamtheta['sd-Intercept'],
+  #   mam_sd_slope=mamtheta['sd-x3'],
+  #   mam_cor=mamtheta['cor'],
+  #   gamm_sd_int=gammtheta['sd-Intercept'],
+  #   gamm_sd_slope=gammtheta['sd-x3'],
+  #   gamm_cor=gammtheta['cor']
+  # )
+ 
+  
+  return(list(fitteddat, preddat))
+}
+
+## Simulations ##
+B <- 2000
+
+simstodo <- expand.grid(
+  K = c(100,200),
+  Nk = c(10,20),
+  conditional=c(FALSE,TRUE),
+  itr = 1:B
+)
+simstodo$simidx <- 1:nrow(simstodo)
+
+simlisttodo <- vector(mode='list',length=nrow(simstodo))
+for (i in 1:length(simlisttodo)) simlisttodo[[i]] <- as.numeric(simstodo[i, ])
+dosim <- function(lst) {
+  cat("Simulation",lst[7],"\n")
+  sim <- tryCatch(do_simulation_misp(K=lst[1],Nk=lst[2],conditional=lst[3],iter=lst[4]),error=function(e) e)
+  sim$sim <- lst[4]
+  sim
+}
+
+cat("Doing",B,"sims.\n")
+tm <- Sys.time()
+sims2 <- mclapply(simlisttodo,dosim)
+
+which_errs <- which(Reduce(c,lapply(sims2,inherits,what='condition')))
+err_messages <- lapply(sims2[which_errs],'[[','message')
+err_messages <- t(as.data.frame(err_messages))
+colnames(err_messages) <- 'message'
+rownames(err_messages) <- NULL
+
+
+
+
+suffix <- gsub('-', '', as.character(Sys.Date()))
+
+save(sims2,file = file.path(savepath,paste0("simsraw-misp-",suffix,".RData")))
+# save(simlist2,file = file.path(savepath,paste0("sims-misp-",suffix,".RData")))
+
+tm2 <- round(as.numeric(difftime(Sys.time(),tm,units='secs')),1)
+cat("Done. It took ",tm2," seconds.\n",sep="")
+
+
+# errs <- !sapply(simlist2[1:numsim],inherits,what='tbl_df')
+errs <- !sapply(sims2,function(x) is.tbl(x[[1]])) ## changed error handling since simlist2 already ignores the NULL elements and screws up the rest
+
+numsim <- length(sims2[!errs]) ## without errors
+simlist2 <- Reduce(rbind,sims2[!errs])
+save(simlist2,file = file.path(savepath,paste0("sims-misp-",suffix,".RData")))
+
+
+simfitted <- bind_rows(simlist2[(0*numsim+(1:numsim))])
+simpred <-   bind_rows(simlist2[(1*numsim+(1:numsim))])
+# simtheta <-  bind_rows(simlist2[(2*numsim+(1:numsim))])
+
+
+###############################################################################
+## Summarize estimation and inference
+# 
+# fitted_x1 <- simfitted %>% filter(x2==0, x3==0)
+# fitted_x2 <- simfitted %>% filter(x1==0, x3==0)
+# alph <- 0.2
+# ## Spaghetti Plots
+# make_spaghetti_plot <- function(K,Nk,sigma0,sigma3,rho,xp) {
+#   if (xp==1) {
+#     rangespag_x1 <- c(-4,4)
+#     
+#     plotdat <- filter(fitted_x1,K==!!K,Nk==!!Nk,sigma0==!!sigma0,sigma3==!!sigma3,rho==!!rho)
+#     
+#     gamplot <- ggplot(plotdat,aes(x1,y=gammargfitted,group=iter)) +
+#       geom_line(colour="gray",alpha=alph)+
+#       geom_line(aes(y=fx1,x=x1),size=1)+
+#       ylim(rangespag_x1)+
+#       ylab("Fitted Curve")+
+#       ggtitle(expression(GAM:~f[1](x[1]))) +
+#       theme(text = element_text(size = GGPLOTTEXTSIZE))
+#     mamplot <- ggplot(plotdat,aes(x1,y=mammargfitted,group=iter)) +
+#       geom_line(colour="gray",alpha=alph)+
+#       geom_line(aes(y=fx1,x=x1),size=1)+
+#       ylim(rangespag_x1)+
+#       ylab("Fitted Curve")+
+#       ggtitle(expression(MAM:~f[1](x[1]))) +
+#       theme(text = element_text(size = GGPLOTTEXTSIZE))
+#     
+#   } else if (xp==2) {
+#     rangespag_x2 <- c(-4,1)
+#     plotdat <- filter(fitted_x2,K==!!K,Nk==!!Nk,sigma0==!!sigma0,sigma3==!!sigma3,rho==!!rho)
+#     
+#     gamplot <- ggplot(plotdat,aes(x2,y=gammargfitted,group=iter)) +
+#       geom_line(colour="gray",alpha=alph)+
+#       geom_line(aes(y=fx2,x=x2),size=1)+
+#       ylim(rangespag_x2)+
+#       ylab("Fitted Curve")+
+#       ggtitle(expression(GAM:~f[2](x[2]))) +
+#       theme(text = element_text(size = GGPLOTTEXTSIZE))
+#     mamplot <- ggplot(plotdat,aes(x2,y=mammargfitted,group=iter)) +
+#       geom_line(colour="gray",alpha=alph)+
+#       geom_line(aes(y=fx2,x=x2),size=1)+
+#       ylim(rangespag_x2)+
+#       ylab("Fitted Curve")+
+#       ggtitle(expression(MAM:~f[2](x[2]))) +
+#       theme(text = element_text(size = GGPLOTTEXTSIZE))
+#   } else {
+#     stop("Unknown covariate.")
+#   }
+#   
+#   list(gam=gamplot,mam=mamplot)
+# }
+# # Save plots to disk
+# uniquesims <- filter(simstodo,itr==1)
+# for (i in 1:nrow(uniquesims)) {
+#   rw <- uniquesims[i, ,drop=FALSE]
+#   for (xp in c(1,2)) {
+#     with(rw,{
+#       tmp <- make_spaghetti_plot(K,Nk,sigma0,sigma3,rho,xp)
+#       nm <- paste0("-K",K,"-Nk",Nk,"-sl",sigma0,"-ss",sigma3,"-r",10*rho,"-x",xp,".pdf")
+#       ggsave(file = file.path(plotpath,paste0("gam",nm)),tmp$gam,width=PLOTWIDTH,height=PLOTHEIGHT)
+#       ggsave(file = file.path(plotpath,paste0("mam",nm)),tmp$mam,width=PLOTWIDTH,height=PLOTHEIGHT)
+#     })
+#   }
+# }
+
+## Summarize Operating Characteristics
+
+covr_x1 <- simfitted %>%
+  filter(x2==0, x3==0) %>%
+  group_by(x1,K,Nk,conditional) %>%
+  summarize(
+    mammargcovr=mean(mammargcovrx1),
+    mamcondcovr=mean(mamcondcovrx1)#,
+    # gammcondcovr=mean(gammcondcovrx1)
+  )
+
+covr_x2 <- simfitted %>%
+  filter(x1==0, x3==0) %>%
+  group_by(x2,K,Nk,conditional) %>%
+  summarize(
+    mammargcovr=mean(mammargcovrx2),
+    mamcondcovr=mean(mamcondcovrx2),
+    # gammcondcovr=mean(gammcondcovrx2)
+  )
+
+# ## Coverage Plots
+# make_coverage_plot <- function(K,Nk,sigma0,sigma3,rho,xp) {
+#   if (xp==1) {
+#     rangecovr_x1 <- c(0.5,1)
+#     
+#     plotdat <- filter(covr_x1,K==!!K,Nk==!!Nk,sigma0==!!sigma0,sigma3==!!sigma3,rho==!!rho)
+#     
+#     gamplot <- ggplot(data=plotdat,aes(x=x1,y=gammargcovr)) +
+#       geom_line() +
+#       ylim(rangecovr_x1) +
+#       ylab("Coverage") +
+#       theme(text = element_text(size = GGPLOTTEXTSIZE))
+#     mamplot <- ggplot(data=plotdat,aes(x=x1,y=mammargcovr)) +
+#       geom_line() +
+#       ylim(rangecovr_x1) +
+#       ylab("Coverage") +
+#     theme(text = element_text(size = GGPLOTTEXTSIZE))
+#     
+#   } else if (xp==2) {
+#     rangecovr_x2 <- c(0.5,1)
+#     plotdat <- filter(covr_x2,K==!!K,Nk==!!Nk,sigma0==!!sigma0,sigma3==!!sigma3,rho==!!rho)
+#     
+#     gamplot <- ggplot(data=plotdat,aes(x=x2,y=gammargcovr)) +
+#       geom_line() +
+#       ylim(rangecovr_x2) +
+#       ylab("Coverage") +
+#     theme(text = element_text(size = GGPLOTTEXTSIZE))
+#     mamplot <- ggplot(data=plotdat,aes(x=x2,y=mammargcovr)) +
+#       geom_line() +
+#       ylim(rangecovr_x2) +
+#       ylab("Coverage") +
+#     theme(text = element_text(size = GGPLOTTEXTSIZE))
+#   } else {
+#     stop("Unknown covariate.")
+#   }
+#   list(gam=gamplot,mam=mamplot)
+# }
+# # Save plots to disk
+# uniquesims <- filter(simstodo,itr==1)
+# for (i in 1:nrow(uniquesims)) {
+#   rw <- uniquesims[i, ,drop=FALSE]
+#   for (xp in c(1,2)) {
+#     with(rw,{
+#       tmp <- make_coverage_plot(K,Nk,sigma0,sigma3,rho,xp)
+#       nm <- paste0("-covr-K",K,"-Nk",Nk,"-sl",sigma0,"-ss",sigma3,"-r",10*rho,"-x",xp,".pdf")
+#       ggsave(file = file.path(plotpath,paste0("gam",nm)),tmp$gam,width=PLOTWIDTH,height=PLOTHEIGHT)
+#       ggsave(file = file.path(plotpath,paste0("mam",nm)),tmp$mam,width=PLOTWIDTH,height=PLOTHEIGHT)
+#     })
+#   }
+# }
+
+### Relative bias in SEs
+
+sesd_x1 <- simfitted %>%
+  filter(x2==0, x3==0) %>%
+  group_by(x1,K,Nk,conditional) %>%
+  summarize(
+    sesd_mammarg = (mean(mammargse)/sd(mammargfitted))-1,
+    sesd_mamcond = (mean(mamcondse)/sd(mamcondfitted))-1
+  )
+
+sesd_x2 <- simfitted %>%
+  filter(x1==0, x3==0) %>%
+  group_by(x2,K,Nk,conditional) %>%
+  summarize(
+    sesd_mammarg = (mean(mammargse)/sd(mammargfitted))-1,
+    sesd_mamcond = (mean(mamcondse)/sd(mamcondfitted))-1
+  )
+
+# make_sesd_plot <- function(K,Nk,sigma0,sigma3,rho,xp) {
+#   if (xp==1) {
+#     rangesesd_x1 <- c(-0.5,0.5)
+#     
+#     plotdat <- filter(sesd_x1,K==!!K,Nk==!!Nk,sigma0==!!sigma0,sigma3==!!sigma3,rho==!!rho)
+#     
+#     gamplot <- ggplot(data=plotdat,aes(x=x1,y=sesd_gam)) +
+#       geom_line() +
+#       ylim(rangesesd_x1) +
+#       ylab("Rel. Bias(SE)") +
+#       theme(text = element_text(size = GGPLOTTEXTSIZE))
+#     mamplot <- ggplot(data=plotdat,aes(x=x1,y=sesd_mam)) +
+#       geom_line() +
+#       ylim(rangesesd_x1) +
+#       ylab("Rel. Bias(SE)") +
+#       theme(text = element_text(size = GGPLOTTEXTSIZE))
+#     
+#   } else if (xp==2) {
+#     rangesesd_x2 <- c(-0.5,0.5)
+#     plotdat <- filter(sesd_x2,K==!!K,Nk==!!Nk,sigma0==!!sigma0,sigma3==!!sigma3,rho==!!rho)
+#     
+#     gamplot <- ggplot(data=plotdat,aes(x=x2,y=sesd_gam)) +
+#       geom_line() +
+#       ylim(rangesesd_x2) +
+#       ylab("Rel. Bias(SE)") +
+#       theme(text = element_text(size = GGPLOTTEXTSIZE))
+#     mamplot <- ggplot(data=plotdat,aes(x=x2,y=sesd_mam)) +
+#       geom_line() +
+#       ylim(rangesesd_x2) +
+#       ylab("Rel. Bias(SE)") +
+#       theme(text = element_text(size = GGPLOTTEXTSIZE))
+#   } else {
+#     stop("Unknown covariate.")
+#   }
+#   list(gam=gamplot,mam=mamplot)
+# }
+# # Save plots to disk
+# uniquesims <- filter(simstodo,itr==1)
+# for (i in 1:nrow(uniquesims)) {
+#   rw <- uniquesims[i, ,drop=FALSE]
+#   for (xp in c(1,2)) {
+#     with(rw,{
+#       tmp <- make_sesd_plot(K,Nk,sigma0,sigma3,rho,xp)
+#       nm <- paste0("-sesd-K",K,"-Nk",Nk,"-sl",sigma0,"-ss",sigma3,"-r",10*rho,"-x",xp,".pdf")
+#       ggsave(file = file.path(plotpath,paste0("gam",nm)),tmp$gam,width=PLOTWIDTH,height=PLOTHEIGHT)
+#       ggsave(file = file.path(plotpath,paste0("mam",nm)),tmp$mam,width=PLOTWIDTH,height=PLOTHEIGHT)
+#     })
+#   }
+# }
+
+
+
+###############################################################################
+## Table for paper
+estimationtable_x1 <- simfitted %>%
+  filter(x2==0,x3==0) %>%
+  group_by(K,Nk,x1,conditional) %>%
+  summarize(
+    bias_x1_mammarg = mean(mammargbiasx1),
+    covr_x1_mammarg = mean(mammargcovrx1),
+    sesd_x1_mammarg = (mean(mammargse)/sd(mammargfitted)),
+    bias_x1_mamcond = mean(mamcondbiasx1),
+    covr_x1_mamcond = mean(mamcondcovrx1),
+    sesd_x1_mamcond = (mean(mamcondse)/sd(mamcondfitted))
+  )%>%
+  group_by(K,Nk,conditional) %>% ## second grouping because now we collapse over x1 (now that we have ratios of SE to SD)
+  summarize_all(mean)
+  
+estimationtable_x2 <- simfitted %>%
+  filter(x1==0,x3==0) %>%
+  group_by(K,Nk,x2,conditional) %>%
+  summarize(
+    bias_x2_mammarg = mean(mammargbiasx2),
+    covr_x2_mammarg = mean(mammargcovrx2),
+    sesd_x2_mammarg = (mean(mammargse)/sd(mammargfitted)),
+    bias_x2_mamcond = mean(mamcondbiasx2),
+    covr_x2_mamcond = mean(mamcondcovrx2),
+    sesd_x2_mamcond = (mean(mamcondse)/sd(mamcondfitted))
+  ) %>%
+  group_by(K,Nk,conditional) %>% ## second grouping because now we collapse over x2 (now that we have ratios of SE to SD)
+  summarize_all(mean)
+
+
+predinttable <- simpred %>%
+  group_by(K,Nk,conditional) %>%
+  summarize(
+    mamRMSEP_int = sqrt(mean(mamMSEPb0,na.rm=TRUE))
+  )
+# varcomptable <- simtheta %>%
+#   group_by(K,Nk,sigma0,sigma3,rho) %>%
+#   summarize(
+#     mam_bias_int = mean(mam_sd_int - sigma0),
+#     gamm_bias_int = mean(gamm_sd_int - sigma0),
+#     
+#     mam_bias_slope = mean(mam_sd_slope - sigma3),
+#     gamm_bias_slope = mean(gamm_sd_slope - sigma3),
+#     
+#     mam_bias_cor = mean(mam_cor - rho),
+#     gamm_bias_cor = mean(gamm_cor - rho)
+#   )
+
+joinvars <- c("K","Nk","conditional")
+resultstable <- estimationtable_x1 %>%
+  left_join(estimationtable_x2,by = joinvars) %>%
+  # left_join(varcomptable,by = joinvars) %>%
+  left_join(predinttable,by = joinvars) 
+
+write_csv(resultstable,file = file.path(plotpath,"numeric-summaries-misp.csv"))
+
+
+cat("Done. Output saved at",globalpath,"\n")
+
+
+# Table 1 (supplememnt)
+resultstable %>%
+  arrange(conditional,K,Nk) %>%
+  mutate(across(contains('covr'),~round(.x*100))) %>%
+  mutate(across(contains('sesd'),~((.x-1)))) %>%
+  dplyr::select(-contains(c("slope","cor"))) %>%
+  dplyr::select(contains(c("K","Nk","conditional",'mammarg'))) %>% 
+  dplyr::select(!contains(c('gamm'))) %>%
+  knitr::kable(
+    digits = 2,
+    format = 'latex' # markdown, not latex, easier copying
+  )
+
+resultstable %>%
+  arrange(conditional,K,Nk) %>%
+  mutate(across(contains('covr'),~round(.x*100))) %>%
+  mutate(across(contains('sesd'),~((.x-1)))) %>%
+  dplyr::select(-contains(c("slope","cor"))) %>%
+  dplyr::select(contains(c("K","Nk","conditional",'mamcond',"RMSEP"))) %>% 
+  dplyr::select(!contains(c('gamm'))) %>%
+  knitr::kable(
+    digits = 2,
+    format = 'latex' # markdown, not latex, easier copying
+  )
+
+
+
+
+
